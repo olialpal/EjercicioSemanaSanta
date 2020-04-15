0c87e344b5f9c34a738bf4551616a991aa2e0f44
Oli <Oli@Oli-PC>
2020-04-15 17:09:14 +0200

    Primera acción con el repositorio

---
+++ EjercicioSemanaSanta/src/Ejer4.java
@@ -0,0 +1,15 @@
+
+public class Ejer4 {
+    public static void main(String[] args){
+        comparacion();
+    }
+    private static void comparacion() {
+        int n=2;
+        int x=6;
+        if (n<x){
+            System.out.println("El primer número es más pequeño que el segundo");
+        }
+            else {
+            System.out.println("El segundo número es más grande que el primero o no");
+        }
+    }}
